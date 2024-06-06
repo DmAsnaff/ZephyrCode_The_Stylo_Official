e697379f40288b3d7d2f1903ea6e8ee6abc3f0ae
amanaazmeer <fathimaamana56@gmail.com>
2024-06-06 20:26:11 +0530

    feat:welcomepage-amana

--- ZephyrCode_Stylo_Expo/app/index.tsx
+++ ZephyrCode_Stylo_Expo/app/index.tsx
@@ -1,314 +1,104 @@
-import { StyleSheet, Image,Text,TextInput, useColorScheme } from 'react-native';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Inputtextname,Colors,Buttoncolor } from '@/constants/Colors';
-import { useThemeColor } from '@/hooks/useThemeColor';
-import { TouchableOpacity } from 'react-native';
-import {Ionicons} from "@expo/vector-icons"
-import { useState } from 'react';
-import Checkbox from 'expo-checkbox';
 import Button from '@/components/buttons';
-import { Link,Stack, useRouter } from 'expo-router';
-import Button2 from '@/components/buttonSecondary';
+import ButtonSecondary from '@/components/buttonSecondary';
+import { useRouter } from 'expo-router';
 
-export default function TabTwoScreen() {
-  const router = useRouter();  // Add this line to get the router instance
-
-  const textColor = useThemeColor({ light: '#11181C', dark: '#ECEDEE' }, 'text');
-  const checkcolor = useThemeColor({ light: Buttoncolor.bblue, dark: Buttoncolor.bgreen }, 'text');
-
-  const [isPasswordShown, setIsPasswordShown] = useState(true)
-  const [isChecked, setisChecked] = useState(false)
+const WelcomeScreen: React.FC = () => {
+  const router = useRouter();
 
   return (
-    
-
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#2C3E50', dark: '#353636' }}
-      headerTitle="Welcome!"
-      headerSubtitle='sign up to continue'>
-  
-      <ThemedView style={{marginBottom:-7}}>
-        <Text style={{
-          color:textColor,
-          fontSize:16,
-          fontWeight:400,
-          marginVertical:8,
-        }}>Full Name</Text>
-
-        <ThemedView style={{
-          width:"100%",
-          height:48,
-          borderColor:Inputtextname.coolgray,
-          borderWidth:1,
-          borderRadius:8,
-          alignItems:"center",
-          justifyContent:"center",
-          paddingLeft:22          
-        }}>
-          
-          <TextInput placeholder='Enter your full name' 
-          placeholderTextColor={Inputtextname.coolgray}
-          keyboardType='email-address'
-          style={{
-            width:"100%",
-            color: textColor,  
-          }}
-          />
-        </ThemedView>
-      </ThemedView>
-
-
-      <ThemedView style={{marginBottom:-7}}>
-        <Text style={{
-          color:textColor,
-          fontSize:16,
-          fontWeight:400,
-          marginVertical:8,
-        }}>User Name </Text>
-
-        <ThemedView style={{
-          width:"100%",
-          height:48,
-          borderColor:Inputtextname.coolgray,
-          borderWidth:1,
-          borderRadius:8,
-          alignItems:"center",
-          justifyContent:"center",
-          paddingLeft:22          
-        }}>
-          
-          <TextInput placeholder='Enter user name' 
-          placeholderTextColor={Inputtextname.coolgray}
-          keyboardType='email-address'
-          style={{
-            width:"100%",
-            color: textColor,  
-          }}
-          />
-        </ThemedView>
-      </ThemedView>
-
-      <ThemedView style={{marginBottom:-7}}>
-        <Text style={{
-          color:textColor,
-          fontSize:16,
-          fontWeight:400,
-          marginVertical:8,
-        }}>Email Address</Text>
-
-        <ThemedView style={{
-          width:"100%",
-          height:48,
-          borderColor:Inputtextname.coolgray,
-          borderWidth:1,
-          borderRadius:8,
-          alignItems:"center",
-          justifyContent:"center",
-          paddingLeft:22          
-        }}>
-          
-          <TextInput placeholder='Enter your Email address' 
-          placeholderTextColor={Inputtextname.coolgray}
-          keyboardType='email-address'
-          style={{
-            width:"100%",
-            color: textColor,  
-          }}
-          />
-        </ThemedView>
-      </ThemedView>
-
-
-      <ThemedView style={{marginBottom:1}}>
-        <Text style={{
-          color:textColor,
-          fontSize:16,
-          fontWeight:400,
-          marginVertical:8,
-        }}>Password</Text>
-
-        <ThemedView style={{
-          width:"100%",
-          height:48,
-          borderColor:Inputtextname.coolgray,
-          borderWidth:1,
-          borderRadius:8,
-          alignItems:"center",
-          justifyContent:"center",
-          paddingLeft:22          
-        }}>
-          
-          <TextInput placeholder='Enter password' 
-          placeholderTextColor={Inputtextname.coolgray}
-          secureTextEntry={isPasswordShown}      
-          style={{
-            width:"100%",
-            color: textColor,  
-             }}
-          />
-
-          <TouchableOpacity
-          onPress={()=>setIsPasswordShown(!isPasswordShown)}
-          style={{
-            right:12,
-            position: "absolute",
-          }}
-          >
-            {
-              isPasswordShown==true ? (
-                <Ionicons name="eye-off" size={24} color={textColor}/>
-
-              ):(
-                <Ionicons name="eye" size={24} color={textColor}/>
-              )
-            }
-          </TouchableOpacity>
-
-
-        </ThemedView>
-      </ThemedView>
-      
-      <ThemedView style={{marginBottom:1}}>
-        <Text style={{
-          color:textColor,
-          fontSize:16,
-          fontWeight:600,
-          marginVertical:1,
-          textAlign:"right",
-          paddingRight:8
-        }}>Forgot Password?</Text>
-      </ThemedView>
-
-
-      <ThemedView style={{
-        flexDirection:'row',
-        marginVertical:6
-      }}>
-
-      <Checkbox
-        style={{marginRight:8}}
-        value={isChecked}
-        onValueChange={setisChecked}
-        color={isChecked ? checkcolor : undefined}>
-      </Checkbox>
-
-      <Text style={{
-          color:textColor,
-          }}>I Accept the </Text>      
-          <Text style={{
-            fontWeight:800,
-            color:textColor,
-            }}>Terms of use</Text> 
-            <Text style={{
-          color:textColor,
-          }}> & </Text> 
-          <Text style={{
-          color:textColor,
-          fontWeight:800,
-          }}>Privacy Policy.</Text>
-
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.logoContainer}>
+        <Image
+          source={require('@/assets/images/the_Stylo_Icon.png')}
+          style={styles.logo}
+          accessibilityLabel="The Stylo Logo"
+        />
       </ThemedView>
 
       <Button
-       title="Sign Up"
-       onPress={() => router.push('/login')} 
-         filled     
-       style={{
-       marginTop:18,
-       marginBottom:4,
-
-      }}>
-
-      </Button>
-
-      <ThemedView style={{ flexDirection:'row', alignItems:'center', marginVertical:20,}}>
-        <ThemedView 
-        style={{ 
-          flex:1,
-          height:1,
-          backgroundColor: Inputtextname.coolgray,
-          marginHorizontal:10,
-          }}/>
-
-        <Text style={{fontSize:14 , color:Inputtextname.coolgray}}>Or Sign Up with </Text>
-        <ThemedView 
-        style={{ 
-          flex:1,
-          height:1,
-          backgroundColor: Inputtextname.coolgray,
-          marginHorizontal:10,
-          }}/>
-      </ThemedView>
-
-      <ThemedView style={{
-        flexDirection: 'row',
-        justifyContent: 'center',
-      }}>
-        <TouchableOpacity
-        onPress={()=> console.log("pressed")}
+        title="LOGIN"
+        onPress={() => router.push('/login')}
+        filled
         style={{
-          alignContent:'center',
-          alignItems:'center',
-          flexDirection:'row',
-          height:52,
-          marginRight:4,
-          borderRadius:10,
-          //paddingHorizontal: 10,
+          marginTop: 18,
+          marginBottom: 4,
+        }}>
 
+        </Button>
 
+      <ButtonSecondary
+        title="SIGN UP"
+        onPress={() => router.push('/register')}
+        filled
+        style={{
+          marginTop: 18,
+          marginBottom: 4,
         }}
-        >
-                <Image 
-                source={require('@/assets/images/google.png')} 
-                style={{ 
-                  height:36,
-                  width:36,
-                  alignSelf:'center',
-                  marginRight:8,
+      >
 
-                }} />
+      </ButtonSecondary>
 
-                <Text  style={{ 
-                color:textColor,
-                  alignItems:'center',
-                  }}>Google </Text>
-        </TouchableOpacity>
+      <ThemedView style={styles.footer}>
+        <ThemedText style={styles.footerText}>
+          Powered by{' '}
+          <ThemedText style={styles.footerTextBold}>ZephyrCode</ThemedText>
+        </ThemedText>
+
+        <Image
+          source={require('@/assets/images/zephyrcode_icon.png')}
+          style={styles.footerLogo}
+          resizeMode="contain"
+          accessibilityLabel="ZephyrCode Logo"
+        />
       </ThemedView>
-      <ThemedView style={{
-          flexDirection:'row',
-          justifyContent:"center",
-          marginVertical:22,
-
-      }}>
-          <Text style={{
-            fontSize:16,
-            color: textColor,
-          }}>Already have an Account?</Text>
-                <Link style={{color:textColor,fontSize:16,fontWeight:800,textDecorationLine: 'underline'}}href="/login"> Sign In</Link>
-
-      </ThemedView>
-
-
-
-
-
-
-    </ParallaxScrollView>
+    </ThemedView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
   },
-  titleContainer: {
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 50,
+    justifyContent: 'center',
+    marginRight: 60,
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 20,
     flexDirection: 'row',
-    gap: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerText: {
+    color: '#7F8C8D',
+    fontSize: 15,
+    marginLeft: 100,
+  },
+  footerTextBold: {
+    fontWeight: 'bold',
+    color: '#2F4F4F',
+  },
+  footerLogo: {
+    width: 100,
+    height: 190,
+    aspectRatio: 2,
+    marginRight: 15,
   },
 });
+
+export default WelcomeScreen;
+
