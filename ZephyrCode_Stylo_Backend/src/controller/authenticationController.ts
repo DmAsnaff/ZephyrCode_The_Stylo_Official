c5324c75b282e9d4e2bf81dfa6b66f3f0848ea17
DmAsnaff <mail2asnaff@gmail.com>
2024-06-13 12:13:40 +0530

    firebase_Error

--- ZephyrCode_Stylo_Backend/src/controller/authenticationController.ts
+++ ZephyrCode_Stylo_Backend/src/controller/authenticationController.ts
@@ -3,18 +3,41 @@ import { dbDisconnector, prisma } from '../prisma/database'
 
 export const registerContoller = async (req: Request, res: Response) => {
     // Request body from the endpoint
-    const body = req.body
+//     const body = req.body
 
-    // Create data by prisma
-   return await prisma.user.create({
-        data: body
-    }).then((primaRes) => {
-       return  res.status(201).json(primaRes)
-    }).catch((err) => {
-        return res.status(500).json({
-            message: err 
-        })
-    }).finally(()=> {
-        dbDisconnector()
+//     // Create data by prisma
+//    return await prisma.user.create({
+//         data: body
+//     }).then((primaRes) => {
+//        return  res.status(201).json(primaRes)
+//     }).catch((err) => {
+//         return res.status(500).json({
+//             message: err 
+//         })
+//     }).finally(()=> {
+//         dbDisconnector()
+//     })
+// }
+
+// console.log('im hitting')
+const {fullName,userName, email, password } = req.body
+// console.log(req.body)
+try {
+    const existingUser = await prisma.user.findUnique({ where: { email: email } })
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already exists' })
+    }
+
+    const newUser = await prisma.user.create({
+      data: { fullName, userName,email,password  }
     })
+
+    res.status(201).json(newUser)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  } finally {
+    dbDisconnector()
+  }
 }
